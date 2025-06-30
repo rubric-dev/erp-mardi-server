81a932c4e0fd87eea9dec8df16aa08c367ffbcf9
jaehyeong2 <wogud1514@gmail.com>
2025-06-30 16:45:03 +0900

    init

---
+++ src/main/java/mardi/erp_mini/ErpMiniApplication.java
@@ -0,0 +1,13 @@
+package mardi.erp_mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mardi/erp_mini/ErpMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package mardi.erp_mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
