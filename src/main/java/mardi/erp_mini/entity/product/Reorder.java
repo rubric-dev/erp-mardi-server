448a3eb3953e75763d875017ceb572e92585a768
Kade <bhpark93@gmail.com>
2025-07-07 16:42:52 +0900

    feat : reorder entity 생성

---
+++ src/main/java/mardi/erp_mini/entity/product/Reorder.java
@@ -0,0 +1,35 @@
+package mardi.erp_mini.entity.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Reorder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long productId;
+    private int quantity;
+
+    private String userId;
+
+    private Boolean isRecommended;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
