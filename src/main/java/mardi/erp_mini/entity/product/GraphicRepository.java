9d7eda76612748bab4e249804f73ab751114c65d
jaehyeong2 <wogud1514@gmail.com>
2025-07-07 15:29:55 +0900

    feature: 엔티티 추가

---
+++ src/main/java/mardi/erp_mini/entity/product/GraphicRepository.java
@@ -0,0 +1,6 @@
+package mardi.erp_mini.entity.product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GraphicRepository extends JpaRepository<Graphic, Long> {
+}
