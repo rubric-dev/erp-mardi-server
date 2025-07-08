6fb4f93338d3d502ad0d8a745a55d792e149468d
jaehyeong2 <wogud1514@gmail.com>
2025-07-08 11:18:53 +0900

    feature: 브랜드-창고 중간 테이블 추가

--- src/main/java/mardi/erp_mini/core/entity/BrandWareHouse.java
+++ src/main/java/mardi/erp_mini/core/entity/BrandWareHouse.java
@@ -1,33 +1,27 @@
 package mardi.erp_mini.core.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class WareHouse {
+public class BrandWareHouse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //창고안에 있는 상품들이 브랜드별로 나뉘는데, 적재되는 곳이 똑같기 때문에 같은 창고라도 브랜드 별로 로우 나뉠 확률이 높음
     private Long brandId;
-    private String name;
-    @Embedded
-    private Address address;
+    private Long wareHouseId;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
 }
-
