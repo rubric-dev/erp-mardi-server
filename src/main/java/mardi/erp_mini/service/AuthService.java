dec98a814630f6a41ec7538ccfdb02d9acf5c71d
Kade <bhpark93@gmail.com>
2025-07-08 17:30:02 +0900

    feat : 사용자 생성 및 로그인 구현

--- src/main/java/mardi/erp_mini/service/AuthService.java
+++ src/main/java/mardi/erp_mini/service/AuthService.java
@@ -3,15 +3,15 @@ package mardi.erp_mini.service;
 import lombok.RequiredArgsConstructor;
 import mardi.erp_mini.common.dto.request.AuthRequest;
 import mardi.erp_mini.common.dto.response.AuthResponse;
-import mardi.erp_mini.core.entity.auth.*;
+import mardi.erp_mini.core.entity.auth.UserAuth;
+import mardi.erp_mini.core.entity.auth.UserAuthRepository;
+import mardi.erp_mini.core.entity.brand.BrandUser;
+import mardi.erp_mini.core.entity.brand.BrandUserRepository;
 import mardi.erp_mini.core.entity.user.User;
-import mardi.erp_mini.core.entity.user.UserRepository;
-import mardi.erp_mini.exception.DuplicateEmailException;
+import mardi.erp_mini.core.entity.user.UserCustomRepository;
 import mardi.erp_mini.exception.NotFoundException;
 import mardi.erp_mini.security.JwtTokenProvider;
 import mardi.erp_mini.security.enums.RoleType;
-import mardi.erp_mini.support.FileUtil;
-import mardi.erp_mini.support.aws.AwsSesClient;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,36 +20,41 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.UUID;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class AuthService {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserAuthRepository userAuthRepository;
-    private final AwsSesClient sesClient;
-    private final VerifyTokenRepository verifyTokenRepository;
     private final PasswordEncoder passwordEncoder;
-//    private final SmsMessageSender messageSender;
-    private final UserRepository userRepository;
-    private final FileUtil fileUtil;
-    private final RolesRepository rolesRepository;
+    private final UserCustomRepository userCustomRepository;
+    private final BrandUserRepository brandUserRepository;
 
-    @Transactional
+    public void createUser(AuthRequest.Create request) {
+        User user = userCustomRepository.createUser(
+                request.getName()
+                ,request.getEmail()
+        );
+
+        userAuthRepository.save(UserAuth.ofUser(request.getEmail(), passwordEncoder.encode(initPassword())));
+
+        brandUserRepository.saveAll(request.getBrandIds().stream()
+                .map(brandId -> BrandUser.builder()
+                        .brandId(brandId)
+                        .userId(user.getId())
+                        .build()
+                )
+                .toList());
+    }
+
+    @Transactional(readOnly = true)
     public AuthResponse.Login loginUser(String email, String password) {
-        Optional<UserAuth> optionalUserAuth = userAuthRepository.findByEmail(email);
-
-        if (optionalUserAuth.isEmpty()) {
-            throw new NotFoundException("not enrolled email");
-        }
-
-        UserAuth userAuth = optionalUserAuth.get();
-        User user = userRepository.findByAuth(userAuth);
+        UserAuth userAuth = userAuthRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("no user found with email: " + email));
 
         if (!passwordEncoder.matches(password, userAuth.getPassword())) {
             throw new BadCredentialsException("Invalid password");
@@ -58,136 +63,16 @@ public class AuthService {
         // 권한 설정
         List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(RoleType.USER.getKey()));
         Authentication authentication = new UsernamePasswordAuthenticationToken(email, password, authorities);
-        return jwtTokenProvider.generateToken(authentication, user.getId(), email);
+        return jwtTokenProvider.generateToken(authentication, userAuth.getId(), email);
     }
 
-    @Transactional
     public AuthResponse.Login refresh(String refreshToken) {
         return jwtTokenProvider.refresh(refreshToken);
     }
 
-    @Transactional
-    public void sendSignUpEmail(AuthRequest.EmailRequest dto) {
-        RoleType role = dto.getType() == VerifyToken.Type.USER_SIGN_UP ? RoleType.USER : RoleType.ADMIN;
+    public String initPassword() {
+        String password = UUID.randomUUID().toString().replaceAll("-", "");
 
-        if (userAuthRepository.existsByEmailAndRole(dto.getEmail(), role)) {
-            throw new DuplicateEmailException();
-        }
-
-        Optional<VerifyToken> existToken = verifyTokenRepository.findByEmailAndType(dto.getEmail(), dto.getType());
-
-        String tokenValue = "";
-
-        if (existToken.isPresent()) {
-            VerifyToken findToken = existToken.get();
-
-            findToken.extendExpiredAt(LocalDateTime.now().plusDays(7));
-            tokenValue = findToken.getValue();
-        } else {
-            VerifyToken verifyToken = VerifyToken.of(dto.getEmail(), dto.getType(), LocalDateTime.now());
-            VerifyToken token = verifyTokenRepository.save(verifyToken);
-
-            tokenValue = token.getValue();
-        }
-
-        sesClient.sendSignUpEmail(dto.getEmail(), tokenValue);
-    }
-
-    @Transactional
-    public AuthResponse.Login signUpByEmail(String token, String password, String name, MultipartFile file) {
-        VerifyToken verifyToken = getTokenOrThrow(token);
-
-        verifyToken.validateExpired();
-
-        String encPassword = passwordEncoder.encode(password);
-
-        UserAuth initUserAuth = UserAuth.ofUser(verifyToken.getEmail(), encPassword);
-        UserAuth userAuth = userAuthRepository.save(initUserAuth);
-
-        String imageUrl = "";
-
-        if (Objects.nonNull(file)) {
-            imageUrl = fileUtil.uploadWithRandomName(file);
-        }
-
-        User user = User.builder()
-                .name(name)
-                .imageUrl(imageUrl)
-                .email(verifyToken.getEmail())
-                .auth(userAuth)
-                .build();
-
-        userRepository.save(user);
-        verifyTokenRepository.delete(verifyToken);
-
-
-        // 권한 설정
-        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(RoleType.USER.getKey()));
-        Authentication authentication = new UsernamePasswordAuthenticationToken(verifyToken.getEmail(), password, authorities);
-        return jwtTokenProvider.generateToken(authentication, user.getId(), verifyToken.getEmail());
-    }
-
-    @Transactional
-    public void sendPasswordResetEmail(AuthRequest.EmailRequest dto) {
-        Optional<User> user = userRepository.findByEmail(dto.getEmail());
-
-        if (user.isEmpty()) throw new NotFoundException("user not found");
-
-        Optional<VerifyToken> existToken = verifyTokenRepository.findByEmailAndType(dto.getEmail(), dto.getType());
-
-        String tokenValue = "";
-
-        if (existToken.isPresent()) {
-            VerifyToken findToken = existToken.get();
-
-            findToken.extendExpiredAt(LocalDateTime.now().plusDays(7));
-            tokenValue = findToken.getValue();
-        } else {
-            VerifyToken verifyToken = VerifyToken.ofPasswordReset(dto.getEmail(), LocalDateTime.now());
-            VerifyToken token = verifyTokenRepository.save(verifyToken);
-
-            tokenValue = token.getValue();
-        }
-
-        sesClient.sendPasswordResetEmail(dto.getEmail(), user.get().getName(), tokenValue);
-    }
-
-    @Transactional
-    public void initPassword(String token, String password) {
-        VerifyToken verifyToken = getTokenOrThrow(token);
-        verifyToken.validateExpired();
-
-        String encPassword = passwordEncoder.encode(password);
-        Optional<UserAuth> userAuth = Optional.empty();
-
-        if (verifyToken.getType() == VerifyToken.Type.USER_PASSWORD_RESET) {
-            userAuth = userAuthRepository.findByEmailAndRole(verifyToken.getEmail(), RoleType.USER);
-        } else userAuthRepository.findByEmailAndRole(verifyToken.getEmail(), RoleType.ADMIN);
-
-        if (userAuth.isEmpty()) throw new NotFoundException("auth not found");
-
-        userAuth.get().modifyPassword(encPassword);
-
-        verifyTokenRepository.delete(verifyToken);
-    }
-
-    private VerifyToken getTokenOrThrow(String token) {
-        return verifyTokenRepository.findByValue(token).orElseThrow(() -> new NotFoundException("token not found"));
-    }
-
-    @Transactional
-    public void sendValidatePhoneMessage(Phone phone) {
-        VerifyToken verifyToken = VerifyToken.ofValidPhone(phone, LocalDateTime.now());
-        verifyTokenRepository.save(verifyToken);
-
-//        messageSender.sendValidatePhoneMessage(phone.getStrippedValue(), verifyToken.getValue());
-    }
-
-    @Transactional
-    public void verifyPhoneBySmsValue(String value) {
-        Optional<VerifyToken> token = verifyTokenRepository.findByValue(value);
-
-        if (token.isEmpty()) throw new NotFoundException("token not found");
-        verifyTokenRepository.delete(token.get());
+        return passwordEncoder.encode(password);
     }
 }
