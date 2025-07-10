fd9eb9a11f1d5d4b57460af509bc038efc972aa8
Kade <bhpark93@gmail.com>
2025-07-10 17:26:28 +0900

    hotfix: DataInitializer profile 추가

--- src/main/java/mardi/erp_mini/config/DataInitializer.java
+++ src/main/java/mardi/erp_mini/config/DataInitializer.java
@@ -17,6 +17,7 @@ import mardi.erp_mini.service.AuthService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import jakarta.persistence.EntityManager;
@@ -26,12 +27,12 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Profile("local")
 @Component
 @RequiredArgsConstructor
 public class DataInitializer {
 
     private final AuthService authService;
-    private final UserAuthRepository userAuthRepository;
     private final BrandRepository brandRepository;
 
     @PostConstruct
