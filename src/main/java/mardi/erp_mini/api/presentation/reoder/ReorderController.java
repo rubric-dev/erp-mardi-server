19aebca3965e189bf6815a0e6561fa2446744822
Kade <bhpark93@gmail.com>
2025-07-11 18:27:40 +0900

    feat: 리오더 홈 mock data 조회 구현

--- src/main/java/mardi/erp_mini/api/presentation/reoder/ReorderController.java
+++ src/main/java/mardi/erp_mini/api/presentation/reoder/ReorderController.java
@@ -1,12 +1,20 @@
 package mardi.erp_mini.api.presentation.reoder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import mardi.erp_mini.api.request.ReorderRequest;
 import mardi.erp_mini.common.dto.response.CommonResponse;
+import mardi.erp_mini.core.response.ReorderResponse;
 import mardi.erp_mini.security.AuthUtil;
 import mardi.erp_mini.service.ReorderService;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -28,6 +36,28 @@ public class ReorderController {
         reorderService.confirm(sessionUserId, id);
         return CommonResponse.ok();
     }
+
+    @Operation(summary = "리오더 홈 화면 mockdata 배포")
+    @GetMapping("/mock")
+    public CommonResponse<List<ReorderResponse.ListRes>> getMockData() {
+        try {
+            ClassPathResource resource = new ClassPathResource("mardi-mockdata.json");
+
+            ObjectMapper objectMapper = new ObjectMapper()
+                    .registerModule(new JavaTimeModule())
+                    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+
+            List<ReorderResponse.ListRes> mockData = objectMapper.readValue(
+                    resource.getInputStream(),
+                    objectMapper.getTypeFactory().constructCollectionType(List.class, ReorderResponse.ListRes.class)
+            );
+            return new CommonResponse<>(mockData);
+        } catch (IOException e) {
+            return CommonResponse.ok();
+        }
+    }
+
 }
 
 
