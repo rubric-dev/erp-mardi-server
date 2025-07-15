9458badbe4ecbf483d94aec66a6c83d2ce807502
Kade <bhpark93@gmail.com>
2025-07-15 16:19:01 +0900

    fix : mockdata 수정 - swagger 설명 추가

--- src/main/java/mardi/erp_mini/api/presentation/scenario/DepletionController.java
+++ src/main/java/mardi/erp_mini/api/presentation/scenario/DepletionController.java
@@ -2,6 +2,7 @@ package mardi.erp_mini.api.presentation.scenario;
 
 
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import mardi.erp_mini.api.request.DepletionRequest;
 import mardi.erp_mini.common.dto.response.CommonResponse;
@@ -16,11 +17,13 @@ public class DepletionController {
 
   private final DepletionService depletionService;
 
+  @Operation(summary = "소진율 설정 목록 조회")
   @GetMapping("/{scenarioId}/{categoryId}")
   public CommonResponse<List<ListRes>> getDepletionLevels(@PathVariable Long scenarioId, @PathVariable Long categoryId){
     return new CommonResponse<>(depletionService.getDepletionLevels(scenarioId, categoryId));
   }
 
+  @Operation(summary = "소진율 구간 범위 수정")
   @PutMapping("/{scenarioId}/{categoryId}")
   public CommonResponse updateDepletionParams(@PathVariable Long scenarioId, @PathVariable Long categoryId, @RequestBody DepletionRequest.UpdateParam request){
     depletionService.updateDepletionLevelParams(scenarioId, categoryId, request);
