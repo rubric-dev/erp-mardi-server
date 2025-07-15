6aafc4e1c24679106f12d976bba3c626ba90b5a0
Kade <bhpark93@gmail.com>
2025-07-15 16:02:55 +0900

    fix : transactional으로 수정

--- src/main/java/mardi/erp_mini/service/DepletionService.java
+++ src/main/java/mardi/erp_mini/service/DepletionService.java
@@ -6,21 +6,21 @@ import mardi.erp_mini.api.request.DepletionRequest;
 import mardi.erp_mini.core.entity.option.*;
 import mardi.erp_mini.core.response.DepletionResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class DepletionService {
-  private final DepletionRepository depletionRepository;
-  private final ScenarioItemRepository scenarioItemRepository;
   private final DepletionDslRepository depletionDslRepository;
 
+  @Transactional(readOnly = true)
   public List<DepletionResponse.ListRes> getDepletionLevels(Long scenarioId, Long categoryId) {
     return depletionDslRepository.getDepeletionLevels(scenarioId, categoryId);
   }
 
+  @Transactional
   public void updateDepletionLevelParams(Long scenarioId, Long categoryId, DepletionRequest.UpdateParam request) {
     ScenarioItem scenarioItem = depletionDslRepository.getScenarioItem(scenarioId, categoryId, request.getDepletionLevelId());
     scenarioItem.updateParams(request.getGreaterThan(), request.getLesserThan());
-    scenarioItemRepository.save(scenarioItem);
-  }
+    }
 }
