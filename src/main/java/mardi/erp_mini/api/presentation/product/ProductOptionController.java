dfc6a3ff38a60ab987e9d5ac35ea24928644e5e6
Kade <bhpark93@gmail.com>
2025-07-15 16:19:01 +0900

    fix : mockdata 수정 - swagger 설명 추가

--- src/main/java/mardi/erp_mini/api/presentation/product/ProductOptionController.java
+++ src/main/java/mardi/erp_mini/api/presentation/product/ProductOptionController.java
@@ -1,5 +1,6 @@
 package mardi.erp_mini.api.presentation.product;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import mardi.erp_mini.common.dto.request.ProductOptionRequest;
 import mardi.erp_mini.common.dto.response.CommonResponse;
@@ -16,24 +17,28 @@ public class ProductOptionController {
     
     private final ProductOptionService productOptionService;
 
+    @Operation(summary = "리오더 설정- 생산 MOQ 목록 조회")
     @PostMapping("/moq")
     public CommonResponse<List<ProductOptionResponse.MoqList>> searchMoqList(@RequestBody ProductOptionRequest.MoqSearchParam searchParam) {
         List<ProductOptionResponse.MoqList> results = productOptionService.getMoqList(searchParam);
         return new CommonResponse<>(results);
     }
 
+    @Operation(summary = "리오더 설정- 생산 MOQ 수정")
     @PutMapping("/moq")
     public CommonResponse updateMoq(@RequestBody ProductOptionRequest.MoqUpdate request) {
         productOptionService.updateMoq(request);
         return CommonResponse.ok();
     }
 
+    @Operation(summary = "리오더 설정- 생산 리드 타임 목록 조회")
     @PostMapping("/leadtime")
     public CommonResponse<List<ProductOptionResponse.LeadTimeList>> searchLeadTimeList(@RequestBody ProductOptionRequest.LeadTimeSearchParam searchParam){
         List<ProductOptionResponse.LeadTimeList> results = productOptionService.getLeadTimeList(searchParam);
         return new CommonResponse<>(results);
     }
 
+    @Operation(summary = "리오더 설정- 생산 리드 타임 수정")
     @PutMapping("/leadtime")
     public CommonResponse updateLeadTime(@RequestBody ProductOptionRequest.LeadTimeUpdate request){
         productOptionService.updateLeadTime(request);
