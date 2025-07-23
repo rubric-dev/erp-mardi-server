848fa1436956e68e375a6b893ed9c21ec8b0e2cd
Kade <bhpark93@gmail.com>
2025-07-23 17:49:00 +0900

    refactor : infoSeason 삭제

--- src/main/java/mardi/erp_mini/service/ProductService.java
+++ src/main/java/mardi/erp_mini/service/ProductService.java
@@ -4,11 +4,9 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mardi.erp_mini.api.request.ProductRequest;
 import mardi.erp_mini.api.request.ProductRequest.SteadySeller;
-import mardi.erp_mini.core.entity.info.InfoSeasonRepository;
 import mardi.erp_mini.core.entity.product.ProductColorRepository;
 import mardi.erp_mini.core.entity.product.ProductCustomRepository;
 import mardi.erp_mini.core.response.ProductResponse;
-import org.apache.poi.util.StringUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
